5154b1f2c6c09ef522a741d614e4d1ae58522cf2
soasme <soasme@gmail.com>
2015-05-04 10:16:45 +0800

    Fix tag generation error.

--- _plugins/gen_tag.rb
+++ _plugins/gen_tag.rb
@@ -10,7 +10,7 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
-      self.data['title'] = "Posts Tagged &ldquo;"+tag+"&rdquo;"
+      self.data['title'] = "Posts Tagged &ldquo; #{tag} &rdquo;"
     end
   end
 
